c0df04ddfe5c8dabaf2f1e6cc1ce2f456ea54470
Aamir Shaikh <aameer.shaikh@mayadata.io>
2023-06-15 23:12:17 -0400

    changed top replies from top 40 to top 20, corrected spelling of top_retweeet

--- PLM_sentiment_analysis.py
+++ PLM_sentiment_analysis.py
@@ -1,15 +1,15 @@
-#Base and Cleaning
+# Base and Cleaning
 import pandas as pd
 
-#Read the csv file into dataframe
+# Read the csv file into dataframe
 file_path = r'C:/Users/Adrienne/Desktop/Amina Laddaoui Undergraduate Research Assistant 22-23/BLM-PLM Research/PLM_tweets_May20_June5,2021_cleaned.csv'
 df = pd.read_csv(file_path)
 
-#Sentiment Analysis
-#make sure you have installed textblob in the terminal first: pip install textblob
+# Sentiment Analysis
+# make sure you have installed textblob in the terminal first: pip install textblob
 from textblob import TextBlob
 
-#Get the sentiments for each tweet
+# Get the sentiments for each tweet
 sentiment = []
 for i in range(len(df)):
     text = df.loc[i, "tweet_text_cleaned"]
@@ -18,100 +18,104 @@ for i in range(len(df)):
     else:
         sentiment.append(TextBlob(str(text)).sentiment.polarity)
 
-   
-#Get the subjectivity of each tweet
-subjectivity=[]
+# Get the subjectivity of each tweet
+subjectivity = []
 for i in range(len(df)):
     subjectivity.append(TextBlob(str(df.loc[i, "tweet_text_cleaned"])).sentiment.subjectivity)
 
-df["sentiment"]=sentiment
-df["subjectivity"]=subjectivity
+df["sentiment"] = sentiment
+df["subjectivity"] = subjectivity
 
-sentimenttotal=0.0
+sentimenttotal = 0.0
 for i in range(len(df)):
-   sentimenttotal+=df.loc[i,"sentiment"]
+    sentimenttotal += df.loc[i, "sentiment"]
 
-subjectivitytotal=0.0
+subjectivitytotal = 0.0
 for i in range(len(df)):
-   subjectivitytotal+=df.loc[i,"subjectivity"]
+    subjectivitytotal += df.loc[i, "subjectivity"]
 
-#Tweets with zero engagement
-count=0
+# Tweets with zero engagement
+count = 0
 for i in range(len(df)):
-  if(df.loc[i,'public_metrics.reply_count']==0 & df.loc[i,'public_metrics.like_count']==0):
-    count+=1
-#total tweets
-print(len(df))
+    if (df.loc[i, 'public_metrics.reply_count'] == 0 & df.loc[i, 'public_metrics.like_count'] == 0):
+        count += 1
+# total tweets
+print("num of total tweets:", len(df))
 # no engagement tweet
-print(count)
-#percent no engagement
-print(count/len(df))
+print("num of tweets with no engagement:", count)
+# percent no engagement
+print("percent of tweets with no engagement:", count / len(df))
 
-sentimentavg= sentimenttotal/len(df)
-subjectivityavg=subjectivitytotal/len(df)
-print("sentiment avg:",sentimentavg)
-print("subjectivity:",subjectivityavg)
+sentimentavg = sentimenttotal / len(df)
+subjectivityavg = subjectivitytotal / len(df)
+print("sentiment avg:", sentimentavg)
+print("subjectivity:", subjectivityavg)
 
-liketotal=0.0
+liketotal = 0.0
 for i in range(len(df)):
-   liketotal+=df.loc[i,"public_metrics.like_count"]
+    liketotal += df.loc[i, "public_metrics.like_count"]
 
-replytotal=0.0
+replytotal = 0.0
 for i in range(len(df)):
-   replytotal+=df.loc[i,"public_metrics.reply_count"]
+    replytotal += df.loc[i, "public_metrics.reply_count"]
 
-retweettotal=0.0
+retweettotal = 0.0
 for i in range(len(df)):
-   retweettotal+=df.loc[i,"public_metrics.retweet_count"]
-   
-quotetotal=0.0
-for i in range(len(df)):
-   quotetotal+=df.loc[i,"public_metrics.quote_count"]
-   
-likeavg= liketotal/len(df)
-replyavg=replytotal/len(df)
-retweetavg=retweettotal/len(df)
-quoteavg=quotetotal/len(df)
-print("like avg:",likeavg)
-print("reply avg:",replyavg)
-print("retweet avg:",retweetavg)
-print("quote avg:",quoteavg)
+    retweettotal += df.loc[i, "public_metrics.retweet_count"]
 
-#remove outliers for likes
+quotetotal = 0.0
+for i in range(len(df)):
+    quotetotal += df.loc[i, "public_metrics.quote_count"]
+
+likeavg = liketotal / len(df)
+replyavg = replytotal / len(df)
+retweetavg = retweettotal / len(df)
+quoteavg = quotetotal / len(df)
+print("like avg:", likeavg)
+print("reply avg:", replyavg)
+print("retweet avg:", retweetavg)
+print("quote avg:", quoteavg)
+
+# remove outliers for likes
 pd_series_likes = df['public_metrics.like_count']
-pd_series_adjusted = pd_series_likes[pd_series_likes.between(pd_series_likes.quantile(.01), pd_series_likes.quantile(.99))]
-df['public_metrics.like_count_no_outliers']=pd_series_adjusted
+pd_series_adjusted = pd_series_likes[
+    pd_series_likes.between(pd_series_likes.quantile(.01), pd_series_likes.quantile(.99))]
+df['public_metrics.like_count_no_outliers'] = pd_series_adjusted
 
-#remove outliers for replies
+# remove outliers for replies
 pd_series_reply = df['public_metrics.reply_count']
-pd_series_adjusted = pd_series_reply[pd_series_reply.between(pd_series_reply.quantile(.01), pd_series_likes.quantile(.99))]
-df['public_metrics.reply_count_no_outliers']=pd_series_adjusted
+pd_series_adjusted = pd_series_reply[
+    pd_series_reply.between(pd_series_reply.quantile(.01), pd_series_likes.quantile(.99))]
+df['public_metrics.reply_count_no_outliers'] = pd_series_adjusted
 
-df.plot(x='sentiment', y='public_metrics.like_count',style='o',figsize=(20,10))
-df.plot(x='sentiment', y='public_metrics.reply_count',style='o',figsize=(20,10))
-df.plot(x='sentiment', y='public_metrics.like_count_no_outliers',style='o',figsize=(20,10))
-df.plot(x='sentiment', y='public_metrics.reply_count_no_outliers',style='o',figsize=(20,10))
+df.plot(x='sentiment', y='public_metrics.like_count', style='o', figsize=(20, 10))
+df.plot(x='sentiment', y='public_metrics.reply_count', style='o', figsize=(20, 10))
+df.plot(x='sentiment', y='public_metrics.like_count_no_outliers', style='o', figsize=(20, 10))
+df.plot(x='sentiment', y='public_metrics.reply_count_no_outliers', style='o', figsize=(20, 10))
 
-#number of posts with positive sentiment
-print((df["sentiment"]>0).sum())
-#percent
-print((df["sentiment"]>0).sum()/len(df))
+# number of posts with positive sentiment
+print("num of posts with positive sentiment:", (df["sentiment"] > 0).sum())
+# percent
+print("percent of posts with positive sentiment:", (df["sentiment"] > 0).sum() / len(df))
 
-#number of posts with neutral sentiment
-print((df["sentiment"]==0).sum())
-#percent
-print((df["sentiment"]==0).sum()/len(df))
+# number of posts with neutral sentiment
+print("num of posts with neutral sentiment:", (df["sentiment"] == 0).sum())
+# percent
+print("percent of posts with neutral sentiment:", (df["sentiment"] == 0).sum() / len(df))
 
-#number of posts with negative sentiment
-print((df["sentiment"]<0).sum())
-#percent
-print((df["sentiment"]<0).sum()/len(df))
+# number of posts with negative sentiment
+print("num of posts with negative sentiment:", (df["sentiment"] < 0).sum())
+# percent
+print("percent of posts with negative sentiment:", (df["sentiment"] < 0).sum() / len(df))
 
-
-df["hashtags"]=df.text.str.lower().str.findall(r'#.*?(?=\s|$)')
+df["hashtags"] = df.text.str.lower().str.findall(r'#.*?(?=\s|$)')
 print(df["hashtags"])
-#method to create a hashtag dictionary
+
+
+# method to create a hashtag dictionary
 def to_1D(series):
- return pd.Series([x for _list in series for x in _list])
-a=to_1D(df["hashtags"]).value_counts().to_string()[:3000]
+    return pd.Series([x for _list in series for x in _list])
+
+
+a = to_1D(df["hashtags"]).value_counts().to_string()[:3000]
 print(a)
